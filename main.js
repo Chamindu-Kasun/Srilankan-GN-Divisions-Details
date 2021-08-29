aefb24563513f3a6f19f2913f270dc0d33b5fa62
Chamindu-Kasun <66759078+Chamindu-Kasun@users.noreply.github.com>
2021-08-30 01:11:36 +0530

    showing all division data

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-let selection = document.querySelector("select");
+$.getJSON("divisions/data.json", function (data) {
+  var division = "";
+  $.each(data, function (key, value) {
+    division += "<tr>";
+    division += "<td>" + value.Province + "</td>";
 
-selection.addEventListener("change", () => {
-  let district = selection.options[selection.selectedIndex].value;
-  if (district == "ampara") {
-    divisionData("divisions/Ampara.json");
-  } else if (district == "anuradhapura") {
-    divisionData("divisions/Anuradhapura.json");
-  } else {
-    console.log("Error");
-  }
+    division += "<td>" + value.District + "</td>";
+
+    division += "<td>" + value.Divisional_Secretariat + "</td>";
+    division +=
+      "<td>" +
+      value.Name_Sinhala +
+      "/" +
+      value.Name_Tamil +
+      "/" +
+      value.Name_English +
+      "</td>";
+    division += "</tr>";
+  });
+  json = data;
+  $("#table tbody").append(division);
 });
 
-function divisionData(division) {
-  $(document).ready(function () {
-    $.getJSON(division, function (data) {
-      var division_data = "";
-      $.each(data, function (key, value) {
-        division_data += "<tr>";
-        division_data += "<td>" + value.District + "</td>";
-        division_data += "<td>" + value.DivisionalSecretariat + "</td>";
-        division_data += "<td>" + value.Name + "</td>";
-        division_data += "</tr>";
-      });
-      $("#table").append(division_data);
-    });
-  });
-}
-window.onload;
+// function filteredTable() {
+//   var results = [];
+//   var District = "District";
+//   var Name = "Name";
+//   var Address = "Address";
+//   var MOH = "MoH";
+//   var searchVal = document.getElementById("search").value.toLowerCase();
+//   $("#table tbody").html("");
+//   for (var i = 0; i < json.length; i++) {
+//     if (json[i][District].toLowerCase().includes(searchVal)) {
+//       filterData();
+//     } else if (json[i][Name].toLowerCase().includes(searchVal)) {
+//       filterData();
+//     } else if (json[i][Address].toLowerCase().includes(searchVal)) {
+//       filterData();
+//     } else if (json[i][MOH].toLowerCase().includes(searchVal)) {
+//       filterData();
+//     }
+//     function filterData() {
+//       var newroutes_data = "";
+//       newroutes_data += "<tr>";
+//       newroutes_data += "<td>" + json[i].District + "</td>";
+//       newroutes_data += "<td>" + json[i].Name + "</td>";
+//       newroutes_data += "<td>" + json[i].Address + "</td>";
+//       newroutes_data += "<td>" + json[i].Phone + "</td>";
+//       newroutes_data += "<td>" + json[i].MoH + "</td>";
+//       newroutes_data += "</tr>";
+
+//       results.push(newroutes_data);
+//       $("#table tbody").append(newroutes_data);
+//     }
+//   }
+// }
